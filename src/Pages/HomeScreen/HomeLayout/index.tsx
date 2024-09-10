97c62b07736d02de86397de1ee8099e0df1adfaf
Pranav Kumar <pranavkumarrajput068@gmail.com>
2024-09-10 12:34:37 +0530

    updated

--- src/Pages/HomeScreen/HomeLayout/index.tsx
+++ src/Pages/HomeScreen/HomeLayout/index.tsx
@@ -21,12 +21,14 @@ const HomeScreen = ({ curimage }: any) => {
   const [innerPageOption, setInnerPageOption]: any = useState("Cover");
   const coverRef = useRef<any>(null);
   const innerRef = useRef<any>(null);
+  const [innerTextColor,setInnerTextColor]:any=useState('')
   const [isContentVisible, setIsContentVisible]: any = useState(false);
   const [previewImage, setPreviewImage]: any = useState({
     cover: null,
     inner: null,
   });
   const [selectedId, setSelectedId] = useState<string | null>(null);
+  const [innercustomised,setInnerCustomised]:any=useState("Text")
 
   useEffect(() => {
     const loadImage: any = new window.Image();
@@ -296,6 +298,152 @@ const HomeScreen = ({ curimage }: any) => {
                       <option value="Lined">Lined</option>
                       <option value="Non Lined">Non Lined</option>
                     </select>
+                    
+                      <div className="lined-content" style={{display:(innerPageOption==='Lined'|| innerPageOption==='Non Lined')?'block':'none'}}>
+                      <label htmlFor="">Select an option</label>
+                      <select name="" id="" onChange={(e:any)=>setInnerCustomised(e.target.value)}
+                        value={innercustomised}>
+                        
+                        <option value="Logo">Upload a logo</option>
+                        <option value="Text">Write some text</option>
+                      </select>
+                    
+                  
+                  
+                 
+                    <div className="uploadlogo" style={{display:innercustomised==='Logo'?"block":"none"}}>
+                    <h4>Upload a logo</h4>
+                    <div className="file" >
+                    <label htmlFor="logoUpload" style={{ cursor: "pointer",marginTop:'10px' }}>
+                       Select a file
+                     </label>
+
+                     <input type="file"style={{ display: "none" }}/>
+                    </div>
+                  </div>
+                
+                     <div style={{display:innercustomised==='Text'?'block':'none'}}>
+                    <div className="input-text">
+                      <input type="text" placeholder="Write some text here" />
+                    </div>
+                    <div className="more-cust">
+                          <h4>Select font style</h4>
+                          <div className="swatch-container">
+                            {/* Repeatable Font Style Items */}
+                            <div className="swatch customily-swatch">
+                              <input
+                                id="font1"
+                                type="radio"
+                                name="fontStyle"
+                                className="needsclick needsfocus"
+                              />
+                              <label
+                                htmlFor="font1"
+                                className="needsclick needsfocus"
+                              >
+                                <img
+                                  src="https://cdn.customily.com/shopify/assetFiles/swatches/may-designs-main.myshopify.com/af1a5c75-f3c9-474f-bc9d-590d16cae0d0/2/15fb2438-ceed-4cef-85e0-5d05506f687e.png"
+                                  loading="lazy"
+                                  width="40"
+                                  height="40"
+                                  alt="font1"
+                                />
+                              </label>
+                            </div>
+                            <div className="swatch customily-swatch">
+                              <input
+                                id="font2"
+                                type="radio"
+                                name="fontStyle"
+                                className="needsclick needsfocus"
+                              />
+                              <label
+                                htmlFor="font2"
+                                className="needsclick needsfocus"
+                              >
+                                <img
+                                  src="https://cdn.customily.com/shopify/assetFiles/swatches/may-designs-main.myshopify.com/af1a5c75-f3c9-474f-bc9d-590d16cae0d0/2/28092ff8-3da0-4fd4-b47b-d15c65cb2daf.png"
+                                  loading="lazy"
+                                  width="40"
+                                  height="40"
+                                  alt="font1"
+                                />
+                              </label>
+                            </div>
+                            <div className="swatch customily-swatch">
+                              <input
+                                id="font3"
+                                type="radio"
+                                name="fontStyle"
+                                className="needsclick needsfocus"
+                              />
+                              <label
+                                htmlFor="font3"
+                                className="needsclick needsfocus"
+                              >
+                                <img
+                                  src="https://cdn.customily.com/shopify/assetFiles/swatches/may-designs-main.myshopify.com/af1a5c75-f3c9-474f-bc9d-590d16cae0d0/2/5481ad74-5b5a-4b1e-9aa1-5d9dab975abc.png"
+                                  loading="lazy"
+                                  width="40"
+                                  height="40"
+                                  alt="font1"
+                                />
+                              </label>
+                            </div>
+                            <div className="swatch customily-swatch">
+                              <input
+                                id="font4"
+                                type="radio"
+                                name="fontStyle"
+                                className="needsclick needsfocus"
+                              />
+                              <label
+                                htmlFor="font4"
+                                className="needsclick needsfocus"
+                              >
+                                <img
+                                  src="https://cdn.customily.com/shopify/assetFiles/swatches/may-designs-main.myshopify.com/af1a5c75-f3c9-474f-bc9d-590d16cae0d0/2/3accc091-642e-4e90-b8d1-310b2333f3af.png"
+                                  loading="lazy"
+                                  width="40"
+                                  height="40"
+                                  alt="font1"
+                                />
+                              </label>
+                            </div>
+                            <div className="swatch customily-swatch">
+                              <input
+                                id="font5"
+                                type="radio"
+                                name="fontStyle"
+                                className="needsclick needsfocus"
+                              />
+                              <label
+                                htmlFor="font5"
+                                className="needsclick needsfocus"
+                              >
+                                <img
+                                  src="https://cdn.customily.com/shopify/assetFiles/swatches/may-designs-main.myshopify.com/af1a5c75-f3c9-474f-bc9d-590d16cae0d0/2/51bba9cb-2e8b-45f9-ab39-b2f87e3e0a3c.png"
+                                  loading="lazy"
+                                  width="40"
+                                  height="40"
+                                  alt="font1"
+                                />
+                              </label>
+                            </div>
+
+                            {/* Add similar font style items */}
+                          </div>
+                        </div>
+                        <div className="more-cust-1">
+                          <h4>Select text color</h4>
+                          <ColorSelector
+                            innerTextColor={innerTextColor}
+                            setInnerTextColor={setInnerTextColor}
+                          />
+                        </div>
+                        </div>
+                        </div>
+                       
                   </div>
                 )}
               </div>
