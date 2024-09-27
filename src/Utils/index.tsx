2b3a319d2fa82e303742cd88adaa5cd53a476f14
harish1907 <nehasharma@tranktechies.com>
2024-09-27 10:56:09 +0530

    update

--- src/Utils/index.tsx
+++ src/Utils/index.tsx
@@ -147,3 +147,25 @@ export const canvasType = {
     },
   ],
 };
+
+export const NumberFormatter = (num: number) => {
+  const lookup = [
+    { value: 1, symbol: "" },
+    { value: 1e3, symbol: "k" },
+    { value: 1e6, symbol: "M" },
+    { value: 1e9, symbol: "B" },
+    { value: 1e12, symbol: "T" },
+    { value: 1e15, symbol: "P" },
+    { value: 1e18, symbol: "E" },
+  ];
+  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+  const item = lookup
+    .slice()
+    .reverse()
+    .find(function (item) {
+      return num >= item.value;
+    });
+  return item
+    ? (num / item.value).toFixed(0).replace(rx, "$1") + item.symbol
+    : "0";
+};
