25c6c04c7176479452ebc9b4ad7d5986c38dc9f2
harish1907 <nehasharma@tranktechies.com>
2024-09-27 17:41:52 +0530

    update

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,31 +3,27 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import { FaRegUserCircle } from "react-icons/fa";
 import { IoCart } from "react-icons/io5";
 
-import './Header.css'
+import "./Header.css";
 import { cartTotal } from "../../store/Services/Product";
 import { useAtom } from "jotai";
 import { cartLengthApiHit } from "../../JotaiStore";
 
 const Header = () => {
   const { pathname } = useLocation();
-  const [cartQty,setCartQty]:any=useState(0);
-  const [apiHitAgain] : any = useAtom(cartLengthApiHit);
-  
-  useEffect(() => {
-    cartTotal().then((res:any)=>{
-      setCartQty(res.count)
-    })
-  }, [apiHitAgain])
+  const [cartQty, setCartQty]: any = useState(0);
+  const [apiHitAgain]: any = useAtom(cartLengthApiHit);
 
+  useEffect(() => {
+    cartTotal().then((res: any) => {
+      setCartQty(res.count);
+    });
+  }, [apiHitAgain]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
-
   }, [pathname]);
 
-
-
-  const navigate=useNavigate()
+  const navigate = useNavigate();
   return (
     <div>
       <header>
@@ -40,17 +36,19 @@ const Header = () => {
               <p>Bulk Journals- journals and notebooks- custom diaries</p>
             </div>
             <div className="cart-header flex space-bw  al-center">
-             
-              <div className="icon-1" onClick={()=>navigate('/account')}>
-                <FaRegUserCircle size={22}/>
-                </div>
-              
-              
-              <div className="flex space-bw al-center icon-2" onClick={()=>navigate('/cart')}>
-                <p><IoCart size={22}/></p>
-                <p>({cartQty})</p>
-                </div>
-            
+              <div className="icon-1" onClick={() => navigate("/account")}>
+                <FaRegUserCircle size={22} />
+              </div>
+
+              <div
+                className="flex space-bw al-center icon-2"
+                onClick={() => navigate("/cart")}
+              >
+                <p>
+                  <IoCart size={22} />
+                </p>
+                <p>{cartQty !== 0 ? `(${cartQty})` : ""}</p>
+              </div>
             </div>
             <div className="toggle">
               <i className="fa-solid fa-bars"></i>
