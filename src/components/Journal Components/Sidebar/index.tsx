8c3dd348a9750061a150a57a83e06732c502dcfa
harish1907 <nehasharma@tranktechies.com>
2024-11-04 17:41:00 +0530

    update

--- src/components/Journal Components/Sidebar/index.tsx
+++ src/components/Journal Components/Sidebar/index.tsx
@@ -15,6 +15,7 @@ const Sidebar = ({
   const [searchInput, setSearchInput]: any = useState("");
   const [filterColors, setFilterColors]: any = useState([]);
   const handleColorChange = (e: any) => {
+    setSearch("");
     const { value, checked } = e.target;
 
     if (checked) {
@@ -26,6 +27,7 @@ const Sidebar = ({
     }
   };
   const handlelinedProducts = (e: any) => {
+    setSearch("");
     const { value, checked } = e.target;
 
     if (checked) {
@@ -37,6 +39,7 @@ const Sidebar = ({
     }
   };
   const handleCoverProducts = (e: any) => {
+    setSearch("");
     const { value, checked } = e.target;
 
     if (checked) {
